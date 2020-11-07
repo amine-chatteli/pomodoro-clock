c5f01898f607d5310e0418bc46ed8b0f0577cd29
amine-chatteli <45702306+amine-chatteli@users.noreply.github.com>
2020-11-08 00:32:01 +0100

    Updates

---
+++ precache-manifest.bed140b60c2e385176bbca8e7bb82e7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57908c5dffdb5d0df17df49730d872b0",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "d9f50f70473c69bfe6aa",
+    "url": "/pomodoro-clock/static/css/main.6ab04c87.chunk.css"
+  },
+  {
+    "revision": "b9d407725102d71a9253",
+    "url": "/pomodoro-clock/static/js/2.b1779938.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pomodoro-clock/static/js/2.b1779938.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d9f50f70473c69bfe6aa",
+    "url": "/pomodoro-clock/static/js/main.e763bc35.chunk.js"
+  },
+  {
+    "revision": "554713b1c155c08808fc",
+    "url": "/pomodoro-clock/static/js/runtime-main.6d59e71d.js"
+  }
+]);
